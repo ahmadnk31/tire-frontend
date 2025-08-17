1ebbcdd6c092b94edfd4c5ead9bbb1b599ce6242
ahmadullah <nikzadahmadullah@gmail.com>
2025-08-17 17:04:56 +0200

    marquee solved

--- src/components/store/Store.tsx
+++ src/components/store/Store.tsx
@@ -101,7 +101,7 @@ export const Store = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <ProductGrid sectionTitle="Featured Products" showAllButton={true} featuredOnly={true} />
       </div>
-      <BrandsMarquee />
+      
     </div>
   );
 };
